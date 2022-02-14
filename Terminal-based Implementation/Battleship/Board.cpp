3ff56ff4f9eb427e18aa4958d7b491ed299f0d3b
Huzaifa Zahid <hzahid99@hotmail.com>
2022-02-13 19:58:11 -0600

    Separated a folder for terminal-based implementation.

---
+++ Terminal-based Implementation/Battleship/Board.cpp
@@ -0,0 +1,353 @@
+#include <iostream>
+#include "Board.h"
+
+using namespace std;
+
+// Creates an empty 10x10 board of water
+// INitializer list
+// idNum in parameter as an alternative to a setter
+Board::Board(int idNum) : m_rows(10), m_cols(10), shipOrientation(0), id(idNum)
+{
+	m_board = new char *[m_rows];
+	for (int i = 0; i < m_rows; i++)
+	{
+		m_board[i] = new char[m_cols];
+	}
+	for (int i = 0; i < m_rows; i++)
+	{
+		for (int j = 0; j < m_cols; j++)
+		{
+			m_board[i][j] = '.';
+		}
+	}
+}
+
+Board::~Board()
+{
+	for (int i = 0; i < m_rows; i++)
+	{
+		delete[] m_board[i];
+	}
+	delete[] m_board;
+}
+
+bool Board::validPlace(int i, int j, int length, int orientation)
+{
+	//if starting outside board
+	if (i < 0 || i > 9 || j < 0 || j > 9 )
+	{
+		return false;
+	}
+	
+
+	//Up
+	if (orientation == 1)
+	{
+		//if ship going outside the board
+		if ((i-length+1) < 0)
+		{
+			return false;
+		}
+		//if a ship already exists
+		for (int z = i-length+1; z <= i; z++)
+		{
+			
+			if (m_board[z][j] == 'S')
+			{
+				return false;
+			}
+			
+		}
+		return true;
+	}
+
+	//Down
+	if (orientation == 2)
+	{
+		//if ship going outside the board
+		if ((i+length-1) > 9)
+		{
+			return false;
+		}
+		//if a ship already exists
+		for (int z = i; z <= i+length-1; z++)
+		{			
+			if (m_board[z][j] == 'S')
+			{
+				return false;
+			}
+			
+		}
+		return true;
+	}
+
+	if (orientation == 3)
+	{
+		//if ship going outside the board
+		if ((j-length+1) < 0)
+		{
+			return false;
+		}
+		//if a ship already exists
+		for (int z = j-length+1; z <= j; z++)
+		{	
+			if (m_board[i][z] == 'S')
+			{
+				return false;
+			}
+			
+		}
+		return true;
+	}
+
+	if (orientation == 4)
+	{
+		//if ship going outside the board
+		if ((j+length-1) > 9)
+		{
+			return false;
+		}
+		//if a ship already exists
+		for (int z = j; z <= j+length-1; z++)
+		{
+			if (m_board[i][z] == 'S')
+			{
+				return false;
+			}
+			
+		}
+		return true;
+	}
+
+}
+
+bool Board::validHit(int i, int j)
+{
+	//If hitting outside board
+	if (i < 0 || i > 9 || j < 0 || j > 9)
+	{
+		return false;
+	}
+	//If hitting at a spot that has already been hit
+	if (m_board[i][j] == 'X' || m_board[i][j] == 'M' )
+	{
+		return false;
+	}
+	
+	
+	return true;
+	
+}
+
+void Board::placeShip(int i, int j, int length, int orientation)
+{
+	m_board[i][j] = 'S';
+	int iPos = i;
+	int jPos = j;
+	shipOrientation = orientation;
+	int shipNum = length-1;
+	
+	// If ship oriented up
+	if (shipOrientation == 1)
+	{
+		//Fill the vectors for the start and end coordinates, will be used in the shipDestroyed()
+		shipIStart.push_back(iPos-length+1);
+		shipIEnd.push_back(iPos);
+		shipJStart.push_back(jPos);
+		shipJEnd.push_back(jPos);
+		//Place S for the entire length of the ship
+		int shipSize = length;
+		while (shipSize != 1)
+		{
+			m_board[iPos - 1][jPos] = 'S';
+			shipSize--;
+			iPos--;
+		}
+	}
+	// If ship oriented down
+	if (shipOrientation == 2)
+	{
+		//Fill the vectors for the start and end coordinates
+		shipIStart.push_back(iPos);
+		shipIEnd.push_back(iPos+length-1);
+		shipJStart.push_back(jPos);
+		shipJEnd.push_back(jPos);
+		//Place S for the entire length of the ship
+		int shipSize = length;
+		while (shipSize != 1)
+		{
+			m_board[iPos + 1][jPos] = 'S';
+			shipSize--;
+			iPos++;
+		}
+	}
+	// If ship oriented left
+	if (shipOrientation == 3)
+	{
+		//Fill the vectors for the start and end coordinates
+		shipIStart.push_back(iPos);
+		shipIEnd.push_back(iPos);
+		shipJStart.push_back(jPos-length+1);
+		shipJEnd.push_back(jPos);
+		//Place S for the entire length of the ship
+		int shipSize = length;
+		while (shipSize != 1)
+		{
+			m_board[iPos][jPos - 1] = 'S';
+			shipSize--;
+			jPos--;
+		}
+	}
+	// If ship oriented right
+	if (shipOrientation == 4)
+	{
+		//Fill the vectors for the start and end coordinates
+		shipIStart.push_back(iPos);
+		shipIEnd.push_back(iPos);
+		shipJStart.push_back(jPos);
+		shipJEnd.push_back(jPos+length-1);
+		//Place S for the entire length of the ship
+		int shipSize = length;
+		while (shipSize != 1)
+		{
+			m_board[iPos][jPos + 1] = 'S';
+			shipSize--;
+			jPos++;
+		}
+	}
+}
+
+bool Board::allShipsSunk()
+{
+	//Nested for loop that checks the whole board for any S
+	for (int i = 0; i < m_rows; i++)
+	{
+		for (int j = 0; j < m_cols; j++)
+		{
+			if (m_board[i][j] == 'S')
+			{
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+
+bool Board::updateBoardHit(int i, int j)
+{
+	//If there is no ship, then display M for miss
+	if (m_board[i][j] == '.')
+	{
+		m_board[i][j] = 'M';
+		return false;
+	}
+	//If there is a ship, then display X for hit
+	else if (m_board[i][j] == 'S')
+	{
+		m_board[i][j] = 'X';
+		return true;
+	}
+}
+
+// invoked after ship is hit
+bool Board::shipDestroyed(int i, int j)
+{
+	//Check which ship has been hit, the indexes of the vector is the ship# (0-4)
+	int shipNum = 0;
+	for (int z = 0; z < shipIStart.size(); z++)
+	{
+		if (i >= shipIStart[z] && i <= shipIEnd[z] && j >= shipJStart[z] && j <= shipJEnd[z])
+		{
+			//Now we know which ship has been hit, and we can use it below to check if the whole ship has been sunk
+			shipNum = z;
+		}
+	}
+
+	 // Ship oriented horizontally
+	if (shipIStart[shipNum] == shipIEnd[shipNum])
+	{
+		//Check if there still is any S left for that ship
+		for (int z = shipJStart[shipNum]; z <= shipJEnd[shipNum]; z++)
+		{
+			//If an S is found, the whole ship hasn't been sunk
+			if (m_board[i][z] == 'S')
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
+	// Ship oriented vertically
+	else if (shipJStart[shipNum] == shipJEnd[shipNum]) 
+	{
+		//Check if there still is any S left for that ship
+		for (int z = shipIStart[shipNum]; z <= shipIEnd[shipNum]; z++)
+		{
+			//If an S is found, the whole ship hasn't been sunk
+			if (m_board[z][j] == 'S')
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+}
+
+void Board::printBoardWOShip()
+{
+	int row = 01;
+	//Print the col heading before printing the array
+	cout << "   A B C D E F G H I J\n";
+	//Print the row headings before printing the next row of the array
+	for (int i = 0; i < m_rows; i++)
+	{
+		if (row != 10)
+		{
+			cout << ' ' << row << ' ';
+			row++;
+		}
+		else
+		{
+			cout << row << ' ';
+		}
+		for (int j = 0; j < m_cols; j++)
+		{
+			if (m_board[i][j] == 'S')
+			{
+				cout << ". ";
+			}
+			else
+			{
+				cout << m_board[i][j] << ' ';
+			}
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
+void Board::printBoardWShip()
+{
+	int row = 01;
+	cout << "   A B C D E F G H I J\n";
+	for (int i = 0; i < m_rows; i++)
+	{
+		if (row != 10)
+		{
+			cout << ' ' << row << ' ';
+			row++;
+		}
+		else
+		{
+			cout << row << ' ';
+		}
+		for (int j = 0; j < m_cols; j++)
+		{
+			cout << m_board[i][j] << ' ';
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
---
+++ Terminal-based Implementation/Battleship/Executive.cpp
@@ -0,0 +1,187 @@
+#include "Executive.h"
+#include <iostream>
+using namespace std;
+
+//Constructor crating 
+Executive::Executive() : player1(1), player2(2)
+{
+}
+
+void Executive::takeNumOfShips()
+{
+    // Taking and validating number of ships
+    cout << "BATTLESHIP!" << endl;
+    cout << "Enter the number of ships you want to play with (1 to 5): ";
+    cin >> numOfShips;
+    // Checking for correct input
+    if (numOfShips < 1 || numOfShips > 5)
+    {
+        //Keep prompting the user to enter a valid num of ships until they do
+        do
+        {
+            cout << "Please enter a valid number of ships: ";
+            cin >> numOfShips;
+        } while (numOfShips < 1 || numOfShips > 5);
+    }
+}
+
+void Executive::placeShips(Board &player)
+{
+
+    // Print Menu, take start row and col
+    for (int i = 1; i <= numOfShips; i++)
+    {
+        //Take the starting col
+        cout << endl << "Player " << player.getId() << ": " << endl;
+        cout << "Enter the following information: " << endl;
+        cout << "Ship #" << i << "\n";
+        cout << "Starting col index (A to J): ";
+        cin >> startColIndex;
+        //Take the staring row
+        startColIndex = startColIndex - 65;
+        cout << "Starting row index (1 to 10): ";
+        cin >> startRowIndex;
+        startRowIndex--;
+
+        // Orient the ship in the specified direction
+        cout << "How would you like to orient your ship?\n";
+        cout << "Up = 1\nDown = 2\nLeft = 3\nRight = 4\n";
+        cout << "Orientation: ";
+        cin >> shipOrientation;
+        if (shipOrientation < 1 || shipOrientation > 4)
+        {
+            do
+            {
+                cout << "PLease enter a valid orientation (1 to 4): ";
+                cin >> shipOrientation;
+            } while (shipOrientation < 1 || shipOrientation > 4);
+            
+        }
+        
+        
+        //Check for bad input
+        if (!player.validPlace(startRowIndex, startColIndex, i, shipOrientation))
+        {
+            //Keep prompting to re-enter the row,col, orientation until they are valid
+            do
+            {
+                cout << "\nShip cannot be placed at these spots. Try again\n";
+                cout << "Ship #" << i << "\n";
+                cout << "Starting col index (A to J): ";
+                cin >> startColIndex;
+
+                startColIndex = startColIndex - 65;
+                cout << "Starting row index (1 to 10): ";
+                cin >> startRowIndex;
+                startRowIndex--;
+
+                // Orient the ship in the specified direction
+                cout << "How would you like to orient your ship?\n";
+                cout << "Up = 1\nDown = 2\nLeft = 3\nRight = 4\n";
+                cout << "Orientation: ";
+                cin >> shipOrientation;
+
+            } while (!player.validPlace(startRowIndex, startColIndex, i, shipOrientation));
+        }
+        //After filtering through the bad input, once the input is valid, place the ships on the board
+        player.placeShip(startRowIndex, startColIndex, i, shipOrientation);
+        //Print the board with the ships so the player can see his own ship placement
+        player.printBoardWShip();
+    }
+}
+
+void Executive::hitMissile(Board &p1, Board &p2)
+{
+    cout << "-----------------------------------------------------------------------------------------------------------------------------------------------------------------\n";
+    //Print the board without the ships
+    cout << "Enemy's board:\n";
+    p2.printBoardWOShip();
+    //Ask the user for col and idex nums
+    cout << "Player " << p1.getId() << " play your move: " << endl;
+    cout << "Enter the column number (A-J) of player" << p2.getId() << "'s box you want to attack : ";
+    cin >> colPos;
+    colPos = colPos - 65;
+    cout << "Enter the row number (1-10) of the player" << p2.getId() << "'s box you want to attack : ";
+    cin >> rowPos;
+    rowPos--;
+
+    //Check if the inputs are invalid
+    if(!p2.validHit(rowPos, colPos))
+    {
+        //Keep prompting the user to re-enter inputs until the inputs are valid
+        do
+        {
+            cout << "Please enter valid row and column: \n";
+            cout << "Enter the column number (A-J) of player" << p2.getId() << "'s box you want to attack : ";
+            cin >> colPos;
+            colPos = colPos - 65;
+            cout << "Enter the row number (1-10) of the player" << p2.getId() << "'s box you want to attack : ";
+            cin >> rowPos;
+            rowPos--;
+            
+        } while (!p2.validHit(rowPos, colPos));
+        
+    }
+
+    // If a ship is hit
+    if (p2.updateBoardHit(rowPos, colPos) == true)
+    {
+        cout << "Hit Successful.\n";
+        // If the hit caused the whole ship to sink
+        if (p2.shipDestroyed(rowPos, colPos))
+        {
+            cout << "Enemy ship destroyed.\n";
+        }
+    }
+    //If it is a miss
+    else
+    {
+        cout << "Hit Missed.\n";
+    }
+    //Print the board again after the hit without displaying the ships
+    cout << "\nEnemy's updated board:\n";
+    p2.printBoardWOShip();
+    //
+    cout << "-----------------------------------------------------------------------------------------------------------------------------------------------------------------\n";
+}
+
+void Executive::run()
+{
+    takeNumOfShips();
+
+    placeShips(player1);
+    for (int i = 0; i < 50; i++)
+    {
+        cout << "-----------------\n";
+    }
+        
+    placeShips(player2);
+    for (int i = 0; i < 50; i++)
+    {
+        cout << "-----------------\n";
+    }
+
+    cout << "Let's destory some ships!" << endl
+         << endl;
+
+    do
+    {
+        hitMissile(player1, player2);
+        hitMissile(player2, player1);
+    } while (!player1.allShipsSunk() && !player2.allShipsSunk());
+
+    if (player1.allShipsSunk() && !player2.allShipsSunk())
+    {
+        cout << "Player 2 has won!" << endl;
+    }
+
+    else if (player2.allShipsSunk() && !player1.allShipsSunk())
+    {
+        cout << "Player 1 has won!" << endl;
+    }
+
+    else
+    {
+        cout << "It is a draw!" << endl;
+    }
+}
